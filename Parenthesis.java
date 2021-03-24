77a5541d1768575fdcc5ce78ee67c572f0a02579
CZY1103 <73876435+CZY1103@users.noreply.github.com>
2021-03-24 20:59:47 +0800

    Add files via upload

--- Parenthesis.java
+++ Parenthesis.java
@@ -1,33 +1,26 @@
-import com.sun.org.apache.xpath.internal.operations.String;
-
-import java.util.*;
+import java.util.Stack;
 
 public class Parenthesis {
-    public static  boolean chkParenthesis(java.lang.String A, int n){
-        if (n%2==1){
+    public boolean chkParenthesis(String A, int n) {
+        Stack<Character> lefts = new Stack<Character>();
+        if(A.isEmpty()||n%2!=0){
             return false;
         }
-        Stack<Character> stack = new Stack<Character>();
-        char[] charArray = A.toCharArray();
-        for (char c:charArray){
-            if (c=='('){
-                stack.push(c);
-            }else if(c == ')'){
-                if(stack.isEmpty()){
+        char[] c = A.toCharArray();
+        for(int i = 0;i < c.length;i++){
+            if(c[i]=='('){
+                lefts.push(c[i]);
+            }else{
+                if(lefts.isEmpty()){
                     return false;
+                }else {
+                    lefts.pop();
                 }
-                stack.pop();
             }
         }
-        if (stack.isEmpty()){
-            return true;
-        } else {
+        if(!(lefts.isEmpty())){
             return false;
         }
-    }
-
-    public static void main(String[] args) {
-        java.lang.String str ="()()(())";
-        System.out.println(chkParenthesis(str,8));
+        return true;
     }
 }
