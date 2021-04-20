f026d1002e43d424324b2c9ceb6346e5790ddb38
CZY1103 <73876435+CZY1103@users.noreply.github.com>
2021-04-20 20:22:44 +0800

    Add files via upload

---
+++ RetirementStock.java
@@ -0,0 +1,16 @@
+public class RetirementStock {
+    public int calculateMax(int[] prices) {
+        int firstBuy = Integer.MAX_VALUE;
+        int firstSell = 0;
+        int secondBuy = Integer.MIN_VALUE;
+        int secondSell = 0;
+
+        for (int price : prices) {
+            firstBuy = Math.min(firstBuy, price);
+            firstSell = Math.max(firstSell, price - firstBuy);
+            secondBuy = Math.max(secondBuy, firstSell - price);
+            secondSell = Math.max(secondSell, secondBuy + price);
+        }
+        return secondSell;
+    }
+}
