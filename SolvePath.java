00b5d30703a1210a023d49a7fb1654c2e5413bd1
CZY1103 <73876435+CZY1103@users.noreply.github.com>
2021-07-14 14:44:43 +0800

    Add files via upload

---
+++ SolvePath.java
@@ -0,0 +1,8 @@
+public class SolvePath {
+    public int uniquePaths (int m, int n) {
+        if(m==1||n==1){
+            return 1;
+        }
+        return uniquePaths(m-1,n)+uniquePaths(m,n-1);
+    }
+}
