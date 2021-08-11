f58bbccd0e5e8602225c7e89a8f0744ee01ea5da
CZY1103 <73876435+CZY1103@users.noreply.github.com>
2021-08-11 20:05:29 +0800

    Add files via upload

--- TumblingHexagon.java
+++ TumblingHexagon.java
@@ -6,9 +6,9 @@ public class TumblingHexagon {
             case 1:
                 return 2 * Math.PI/6;
             case 2:
-                return 2*Math.PI*2/6;
+                return 2*Math.PI*Math.sqrt(3)/6;
             case 3:
-                return 2;
+                return 2*Math.PI*2/6;
         }
         return 0;
     }
@@ -21,17 +21,23 @@ public class TumblingHexagon {
             case 3:
                 return radian(1)+radian(2)+radian(3);
             case 4:
-                return radian(1)+radian(2)+radian(3)*2;
+                return radian(1)+radian(2)*2+radian(3);
             case 5:
-                return radian(1)+radian(2)*2+radian(3)*2;
             case 6:
-                return radian(1)*2+radian(2)*2+radian(3)*2;
+                return radian(1)*2+radian(2)*2+radian(3);
         }
         return 0;
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int k = sc.nextInt();
+        int n = k%6;
+        int m = k/6;
+        if (n==0){
+            System.out.println(radianAll(6)*m);
+        }else{
+            System.out.println(radianAll(6)*m+radianAll(n));
+        }
 
     }
 }
